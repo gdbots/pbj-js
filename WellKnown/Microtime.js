3e4ce36989e0f0ddc27e32b632fd130ce76c6fc2
Greg Brown <greg@12dnetworks.com>
2017-05-29 17:39:44 -0700

    added WellKnown/Microtime with unit tests

---
+++ WellKnown/Microtime.js
@@ -0,0 +1,122 @@
+import isString from 'lodash-es/isString';
+import moment from 'moment';
+import padEnd from 'lodash-es/padEnd';
+import AssertionFailed from '../Exception/AssertionFailed';
+
+/**
+ * Value object for microtime with methods to convert to and from integers.
+ * Note that this is a unix timestamp __WITH__ microseconds but stored
+ * as an integer NOT a float.
+ *
+ * In the PHP lib we have 10 digits (unix timestamp) concatenated with
+ * 6 microsecond digits (from php's microtime function we get that value).
+ * In JavaScript we don't get microsecond precision, we get milliseconds.
+ * fixme: generate microseconds precision to match php.
+ *
+ * @link http://php.net/manual/en/function.microtime.php
+ * @link https://github.com/gdbots/pbj-php/blob/master/src/WellKnown/Microtime.php
+ */
+export default class Microtime {
+  /**
+   * @param {string} value
+   *
+   * @throws {AssertionFailed}
+   */
+  constructor(value) {
+    if (!isString(value)) {
+      throw new AssertionFailed('Microtime value must be a string.');
+    }
+
+    if (!/^[0-9]{16}$/.test(value)) {
+      throw new AssertionFailed('Microtime must be 16 digits.');
+    }
+
+    Object.defineProperty(this, 'value', { value });
+    Object.freeze(this);
+  }
+
+  /**
+   * @returns {Microtime}
+   */
+  static create() {
+    return this.fromMoment(moment());
+  }
+
+  /**
+   * @param {Date} date
+   *
+   * @returns {Microtime}
+   */
+  static fromDate(date) {
+    return this.fromMoment(moment(date));
+  }
+
+  /**
+   * @param {string} value
+   *
+   * @returns {Microtime}
+   */
+  static fromString(value) {
+    return new Microtime(value);
+  }
+
+  /**
+   * @param {moment.Moment} m
+   *
+   * @returns {Microtime}
+   */
+  static fromMoment(m) {
+    return new Microtime(`${padEnd(m.valueOf(), 16, '0')}`);
+  }
+
+  /**
+   * @returns {number}
+   */
+  toNumber() {
+    return +`${this.value.substr(0, 10)}.${this.value.substr(10)}`;
+  }
+
+  /**
+   * @returns {moment.Moment}
+   */
+  toMoment() {
+    return moment.unix(this.toNumber());
+  }
+
+  /**
+   * @returns {Date}
+   */
+  toDate() {
+    return this.toMoment().toDate();
+  }
+
+  /**
+   * @returns {string}
+   */
+  toString() {
+    return this.value;
+  }
+
+  /**
+   * @returns {string}
+   */
+  toJSON() {
+    return this.value;
+  }
+
+  /**
+   * @returns {string}
+   */
+  valueOf() {
+    return this.value;
+  }
+
+  /**
+   * @param {Microtime} other
+   *
+   * @returns {boolean}
+   */
+  equals(other) {
+    return this.value === other.value;
+  }
+}
