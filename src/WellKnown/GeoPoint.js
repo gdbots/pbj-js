b19b706a163a5dbb202e3a501b6f8db7f4e94118
Greg Brown <greg@12dnetworks.com>
2017-05-30 19:19:59 -0700

    added WellKnown GeoPoint with unit tests

---
+++ src/WellKnown/GeoPoint.js
@@ -0,0 +1,116 @@
+import isArray from 'lodash/isArray';
+import toFinite from 'lodash/toFinite';
+import AssertionFailed from '../Exception/AssertionFailed';
+
+/**
+ * Represents a GeoJson Point value.
+ * @link http://geojson.org/geojson-spec.html#point
+ */
+export default class GeoPoint {
+  /**
+   * @param {number} lat
+   * @param {number} lon
+   *
+   * @throws {AssertionFailed}
+   */
+  constructor(lat, lon) {
+    const flat = toFinite(toFinite(lat).toFixed(8));
+    const flon = toFinite(toFinite(lon).toFixed(8));
+
+    if (flat > 90.0 || flat < -90.0) {
+      throw new AssertionFailed(`Latitude "${flat}" must be within range [-90.0, 90.0]`);
+    }
+
+    if (flon > 180.0 || flon < -180.0) {
+      throw new AssertionFailed(`Longitude "${flon}" must be within range [-180.0, 180.0]`);
+    }
+
+    Object.defineProperty(this, 'lat', { value: flat });
+    Object.defineProperty(this, 'lon', { value: flon });
+    Object.freeze(this);
+  }
+
+  /**
+   * @param {string} value
+   *
+   * @returns {GeoPoint}
+   */
+  static fromString(value) {
+    const p = value.split(',');
+    return new this(p[0], p[1]);
+  }
+
+  /**
+   * @param {string} json
+   *
+   * @returns {GeoPoint}
+   */
+  static fromJSON(json) {
+    let obj;
+
+    try {
+      obj = JSON.parse(json);
+    } catch (e) {
+      throw new AssertionFailed(`Payload "${json}" is not valid JSON.`);
+    }
+
+    return this.fromObject(obj);
+  }
+
+  /**
+   * @param {Object} obj
+   *
+   * @returns {GeoPoint}
+   */
+  static fromObject(obj) {
+    if (obj.coordinates && isArray(obj.coordinates) && obj.coordinates.length === 2) {
+      return new GeoPoint(obj.coordinates[1], obj.coordinates[0]);
+    }
+
+    throw new AssertionFailed(`Object [${JSON.stringify(obj)}] is not a valid GeoJson "Point" type.`);
+  }
+
+  /**
+   * @returns {number}
+   */
+  getLatitude() {
+    return this.lat;
+  }
+
+  /**
+   * @returns {number}
+   */
+  getLongitude() {
+    return this.lon;
+  }
+
+  /**
+   * @returns {string}
+   */
+  toString() {
+    return `${this.lat},${this.lon}`;
+  }
+
+  /**
+   * @returns {Object}
+   */
+  toJSON() {
+    return { type: 'Point', coordinates: [this.lon, this.lat] };
+  }
+
+  /**
+   * @returns {string}
+   */
+  valueOf() {
+    return this.toString();
+  }
+
+  /**
+   * @param {GeoPoint} other
+   *
+   * @returns {boolean}
+   */
+  equals(other) {
+    return this.toString() === other.toString();
+  }
+}
---
+++ tests/WellKnown/GeoPoint.test.js
@@ -0,0 +1,91 @@
+import test from 'tape';
+import GeoPoint from '../../src/WellKnown/GeoPoint';
+
+test('GeoPoint new tests', (t) => {
+  const gp = new GeoPoint(0.5, 102.0);
+  t.true(gp instanceof GeoPoint);
+  t.true(gp.equals(new GeoPoint(gp.getLatitude(), gp.getLongitude())));
+  t.true(gp.equals(GeoPoint.fromString(`${gp}`)));
+  t.true(gp.equals(GeoPoint.fromJSON(JSON.stringify(gp))));
+
+  t.same(gp.getLatitude(), 0.5);
+  t.same(gp.getLongitude(), 102);
+  t.same(`${gp.getLatitude()}`, '0.5');
+  t.same(`${gp.getLongitude()}`, '102');
+  t.same(gp.toString(), '0.5,102');
+  t.same(gp.toJSON(), { type: 'Point', coordinates: [102, 0.5] });
+  t.same(JSON.stringify(gp), '{"type":"Point","coordinates":[102,0.5]}');
+
+  try {
+    gp.test = 1;
+    t.fail('gp instance is mutable');
+  } catch (e) {
+    t.pass('gp instance is immutable');
+  }
+
+  t.end();
+});
+
+
+test('GeoPoint fromString tests', (t) => {
+  const gp = GeoPoint.fromString('34.1789335,-118.347594');
+  t.true(gp instanceof GeoPoint);
+  t.true(gp.equals(new GeoPoint(34.1789335, -118.347594)));
+  t.true(gp.equals(GeoPoint.fromString(`${gp}`)));
+  t.true(gp.equals(GeoPoint.fromJSON(JSON.stringify(gp))));
+
+  t.same(gp.getLatitude(), 34.1789335);
+  t.same(gp.getLongitude(), -118.347594);
+  t.same(`${gp.getLatitude()}`, '34.1789335');
+  t.same(`${gp.getLongitude()}`, '-118.347594');
+  t.same(gp.toString(), '34.1789335,-118.347594');
+  t.same(gp.toJSON(), { type: 'Point', coordinates: [-118.347594, 34.1789335] });
+  t.same(JSON.stringify(gp), '{"type":"Point","coordinates":[-118.347594,34.1789335]}');
+  t.end();
+});
+
+
+test('GeoPoint fromJSON tests', (t) => {
+  const gp = GeoPoint.fromJSON('{"type":"Point","coordinates":[-118.347594,34.1789335]}');
+  t.true(gp instanceof GeoPoint);
+  t.true(gp.equals(new GeoPoint(34.1789335, -118.347594)));
+  t.true(gp.equals(GeoPoint.fromString(`${gp}`)));
+  t.true(gp.equals(GeoPoint.fromJSON(JSON.stringify(gp))));
+
+  t.same(gp.getLatitude(), 34.1789335);
+  t.same(gp.getLongitude(), -118.347594);
+  t.same(`${gp.getLatitude()}`, '34.1789335');
+  t.same(`${gp.getLongitude()}`, '-118.347594');
+  t.same(gp.toString(), '34.1789335,-118.347594');
+  t.same(gp.toJSON(), { type: 'Point', coordinates: [-118.347594, 34.1789335] });
+  t.same(JSON.stringify(gp), '{"type":"Point","coordinates":[-118.347594,34.1789335]}');
+  t.end();
+});
+
+
+test('GeoPoint fromJSON(invalid) tests', (t) => {
+  const invalid = [
+    '[-118.347594,34.1789335]', // not even GeoJson
+    '{"type":"Point","coordinates":[34.1789335,-118.347594]}', // wrong order lat/long
+    '{"type":"Point","coordinates":[190,-100]}',
+    'a,b',
+    'nope',
+    false,
+    [],
+    {},
+    '',
+    NaN,
+    undefined,
+  ];
+
+  invalid.forEach((value) => {
+    try {
+      GeoPoint.fromJSON(value);
+      t.fail(`GeoPoint.fromJSON accepted invalid value [${JSON.stringify(value)}].`);
+    } catch (e) {
+      t.pass(e.message);
+    }
+  });
+
+  t.end();
+});
